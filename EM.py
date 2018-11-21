e3fd536dd6c13416dad1710e9a60a2c46d36214c
tmclouisluk <failuk333@hotmail.com>
2018-11-21 18:00:58 +0800

    update EM

---
+++ EM.py
@@ -0,0 +1,165 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+import numpy as np
+import scipy.stats
+import math
+import cv2
+from scipy import ndimage
+
+from sklearn import cluster
+from scipy.cluster.vq import kmeans2
+import matplotlib.pyplot as plt
+from scipy import ndimage
+
+
+# input fllename >> output 3d array
+def read_img(filename):
+    img_3d = ndimage.imread(filename)
+
+    small = cv2.resize(img_3d, (0, 0), fx=0.1, fy=0.1)
+    blur = cv2.blur(small, (2, 2))
+    return blur
+
+
+# input 3d array >> output 2d array
+def flatten_img(img_3d):
+    x, y, z = img_3d.shape
+    img_2d = img_3d.reshape(x*y, z)
+    img_2d = np.array(img_2d, dtype = np.float)
+    return img_2d
+
+
+# input 2d array >> output 3d array
+def recover_img(img_2d, vis = False, X=800, Y=800, Z=3):
+    #img_2d = cv2.resize(img_2d, (0, 0), fx=10, fy=10)
+    recover_img = img_2d.reshape(X*0.1, Y*0.1, Z)
+    return recover_img
+
+
+# input 2d array >> output estimated means, stds, pis
+def kmeans_init(img, k):
+    means, labels = kmeans2(img, k)
+    means = np.array(means)
+    cov = np.array([np.cov(img[labels == i].T) for i in range(k)])
+    ids = set(labels)
+    pis = np.array([np.sum([labels == i]) / len(labels) for i in ids])
+    return means, cov, pis
+
+
+# E-Step: Update Parameters
+# update the conditional pdf - prob that pixel i given class j
+def update_responsibility(img, means, cov, pis, k):
+    # responsibilities: i th pixels, j th class
+    # pis * gaussian.pdf
+    responsibilities = np.array([pis[j] * scipy.stats.multivariate_normal.pdf(img, mean=means[j], cov=cov[j]) for j in range(k)]).T
+    # normalize for each row
+    norm = np.sum(responsibilities, axis = 1)
+    # convert to column vector
+    norm = np.reshape(norm, (len(norm), 1))
+    responsibilities = responsibilities / norm
+    return responsibilities
+
+
+# update pi for each class of Gaussian model
+def update_pis(responsibilities):
+    pis = np.sum(responsibilities, axis = 0) / responsibilities.shape[0]
+    return pis
+
+
+# update means for each class of Gaussian model
+def update_means(img, responsibilities):
+    means = []
+    class_n = responsibilities.shape[1]
+    for j in range(class_n):
+        weight = responsibilities[:, j] / np.sum(responsibilities[:, j])
+        weight = np.reshape(weight, (1, len(weight)))
+        means_j = weight.dot(img)
+        means.append(means_j[0])
+    means = np.array(means)
+    return means
+
+
+# update covariance matrix for each class of Gaussian model
+def update_covariance(img, responsibilities, means):
+    cov = []
+    class_n = responsibilities.shape[1]
+    for j in range(class_n):
+        weight = responsibilities[:, j] / np.sum(responsibilities[:, j])
+        weight = np.reshape(weight, (1, len(weight)))
+        # Each pixels have a covariance matrice
+        covs = [np.mat(i - means[j]).T * np.mat(i - means[j]) for i in img]
+        # Weighted sum of covariance matrices
+        cov_j = sum(weight[0][i] * covs[i] for i in range(len(weight[0])))
+        cov.append(cov_j)
+    cov = np.array(cov)
+    return cov
+
+
+# M-step: choose a label that maximise the likelihood
+def update_labels(responsibilities):
+    labels = np.argmax(responsibilities, axis = 1)
+    return labels
+
+
+def update_loglikelihood(img, means, cov, pis, k):
+    pdf = np.array([pis[j] * scipy.stats.multivariate_normal.pdf(img, mean=means[j], cov=cov[j]) for j in range(k)])
+    log_ll = np.log(np.sum(pdf, axis = 0))
+    log_ll_sum = np.sum(log_ll)
+    return log_ll_sum
+
+
+def EM_cluster(img, k, error = 10e-4, iter_n = 9999):
+    #  init setting
+    cnt = 0
+    likelihood_arr = []
+    # Initialise E-Step by KMeans
+    means, cov, pis = kmeans_init(img, k)
+    likelihood = 0
+    new_likelihood = 2
+    responsibilities = update_responsibility(img, means, cov, pis, k)
+    while (abs(likelihood - new_likelihood) > error) and (cnt != iter_n):
+        start_dt = datetime.datetime.now()
+        cnt += 1
+        likelihood = new_likelihood
+        # M-Step
+        labels = update_labels(responsibilities)
+        # E-step
+        responsibilities = update_responsibility(img, means, cov, pis, k)
+        means = update_means(img, responsibilities)
+        cov = update_covariance(img, responsibilities, means)
+        pis = update_pis(responsibilities)
+        new_likelihood = update_loglikelihood(img, means, cov, pis, k)
+        likelihood_arr.append(new_likelihood)
+        end_dt = datetime.datetime.now()
+        diff = relativedelta(end_dt, start_dt)
+        print("iter: %s, time interval: %s:%s:%s:%s" % (cnt, diff.hours, diff.minutes, diff.seconds, diff.microseconds))
+    likelihood_arr = np.array(likelihood_arr)
+    print('Converge at iteration {}'.format(cnt + 1))
+    return labels, means, cov, pis, likelihood_arr
+
+
+def main():
+    FILENAME1 = './img/Co6q3lrygK-AbzQ0AAKvDylHHq4008.jpg'
+    FILENAME2 = './img/Co6q6Fry23KAGdrRAAJBxxyYEHQ922.jpg'
+    try:
+        orig_img = read_img(FILENAME2)
+        plt.figure()
+        plt.axis("off")
+        plt.imshow(orig_img)
+        plt.title('Original Image');
+        plt.show()
+
+        img = flatten_img(orig_img)
+        labels, means, cov, pis, likelihood_arr = EM_cluster(img, 5)
+        em_img = recover_img(means[labels])
+        plt.figure()
+        plt.axis("off")
+        plt.imshow(em_img)
+        plt.title('Image Segmented by EM Algorithm');
+        plt.show()
+    except Exception as ex:
+        print(ex)
+
+
+main()
